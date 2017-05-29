34f18e4d0fdc08a2ef500c1c61314a7d6f7d3664
stasw <stasw@PREINST080513B>
2017-05-29 11:21:49 +0300

    לל

--- HouseColoring/Source.cpp
+++ HouseColoring/Source.cpp
@@ -2,9 +2,10 @@
 #include <random>
 #include <vector>
 #include <functional>
-
+#include <set>
 using namespace std;
 using Cost = int;
+using Color = int;
 void PrintSpaced(int Number,int Area)
 {
 	int Digits = (int)log10(Number) + 1;
@@ -32,9 +33,17 @@ void main()
 		}
 		cout << endl;
 	}*/
-	Houses[0] = {1,1  ,1};
-	Houses[1] = {2,100,2};
-	Houses[2] = {3,100,3};
+	Houses[0] = {1,1  ,1,1};
+	Houses[1] = {2,100,2,100};
+	Houses[2] = {3,100,3,8};
+	for (int i = 0; i < 3; i++)
+	{
+		for (auto& val : Houses[i])
+		{
+			PrintSpaced(val, 4);
+		}
+		cout << endl;
+	}
 	auto FindMin = [](vector<int> args) -> auto {
 		int minCost = args[0];
 		int optimalIndex = 0;
@@ -52,7 +61,13 @@ void main()
 	{
 		vector<int> House;
 		vector<int> Cost;
+		vector<int> Color;
 		int Total;
+	};
+	enum eColors {
+		Yellow, 
+		Blue, 
+		Green
 	};
 	function<Coloring(int,int)> Choose = [&](int i,int Restriction) -> Coloring {
 		if (i > Houses[0].size() - 1)
@@ -62,43 +77,31 @@ void main()
 		}
 		int iYellow, iBlue, iGreen;
 		vector<int> Array;
-		Coloring coloring;
-		if (Restriction != 0)
+		vector<Coloring> coloring;
+		set<Color> ColorsAvailable = { Yellow,Blue,Green };
+		if (ColorsAvailable.find(Restriction) != ColorsAvailable.end())
+			ColorsAvailable.erase(ColorsAvailable.find(Restriction));
+		
+		for(auto c : ColorsAvailable)
 		{
-			coloring = Choose(i + 1, 0);
-			iYellow = coloring.Total + Houses[0][i];
-		}
-		if (Restriction != 1)
-		{
-			coloring = Choose(i + 1, 1);
-			iBlue = coloring.Total + Houses[1][i];
-		}
-		if (Restriction != 2)
-		{
-			coloring = Choose(i + 1, 2);
-			iGreen = coloring.Total + Houses[2][i];
+			coloring.push_back(Choose(i + 1, c));
+			coloring.back().Total += Houses[c][i];
+			coloring.back().Cost.push_back(Houses[c][i]);
+			coloring.back().House.push_back(i);
+			coloring.back().Color.push_back(c);
+			Array.push_back(coloring.back().Total);
 		}
 
-		if (Restriction == 0)
-			Array = { iBlue, iGreen };
-		if (Restriction == 1) 
-			Array = { iYellow, iGreen };
-		if (Restriction == 2) 
-			Array = { iYellow, iBlue };
-		if (Restriction == 4)
-			Array = { iYellow , iBlue, iGreen};
 		pair<int,int> Min = FindMin(Array);
-		PrintSpaced(Min.first, 4);
-		coloring.House.push_back( Min.second );
-		coloring.Cost.push_back(Houses[Min.second][i] );
-		coloring.Total = Array[Min.second];
-		return coloring;
+		return coloring[Min.second];
 	};
 	Coloring returnValue = Choose(0,4);
 	int total = 0;
-	for (auto c : returnValue.Cost)
+	cout << endl;
+	for (int i = 0; i < returnValue.House.size(); i++)
 	{
-		total += c;
+		cout << "House number " << returnValue.House[i] << " With color " << returnValue.Color[i] <<
+			" With Cost of " << returnValue.Cost[i] << endl;
 	}
 	cout << endl << "Optimal Cost : " << returnValue.Total << endl;
 	cin.get();
